a8999269f7e46d50a7393be381f306c3a6b68cd6
Patsakula Nikita <pacakula@kribrum.ru>
2022-11-09 16:23:47 +0300

    SQUASH

--- tools.nix
+++ tools.nix
@@ -236,6 +236,7 @@ rec {
               submodules = true;
               inherit (parsed) url rev;
               ref = attrs.branch or "master";
+              allRefs = true;
             };
             hash = pkgs.runCommand "hash-of-${attrs.name}" { nativeBuildInputs = [ pkgs.nix ]; } ''
               echo -n "$(nix-hash --type sha256 ${src})" > $out
